ca817653bca0c653ecba096042e90fded339a814
Victoria Limberger <limberger.v02@htlwienwest.at>
2020-05-12 14:50:54 +0200

    6 Spalten wurden erstellt

---
+++ dbo.Table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Buecher]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Titel] VARCHAR(50) NULL, 
+    [Author] VARCHAR(50) NULL, 
+    [Kurzbeschreibung] VARCHAR(50) NULL, 
+    [Kategorie] VARCHAR(20) NULL, 
+    [Verlag] VARCHAR(30) NULL
+)
